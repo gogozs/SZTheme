a40435617200e9dce56d4c94831df709fa7bdea2
Song Zhou <zhousong1993@gmail.com>
2018-04-11 19:53:39 +0800

    Initial Commit

---
+++ SZTheme/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SZTheme
+//
+//  Created by songzhou on 2018/4/11.
+//  Copyright © 2018年 songzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
